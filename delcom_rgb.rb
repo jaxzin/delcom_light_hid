c98ca92ae19c9e1cc7f9fe1a3ccc0d21b2b97ff8
Gregory McIntyre <blue.puyo@gmail.com>
2011-08-09 17:46:01 +1000

    Renamed to delcome_rgb and fixed colours.

--- delcom_rgb.rb
+++ delcom_rgb.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
-# Original author: Ian Leitch <ian@envato.com>, Copyright 2010 Envato
-# Extra light colour combinations: Gregory McIntyre <blue.puyo@gmail.com>
+# RGY original: Ian Leitch <ian@envato.com>, Copyright 2010 Envato
+# RGB version: Gregory McIntyre <blue.puyo@gmail.com>
 #
 # You'll need to install ruby-usb:
 #
@@ -17,31 +17,18 @@ class Delcom
   DEFAULT_VENDOR_ID = 0x0fc5
   DEFAULT_PRODUCT_ID = 0xb080
   DEFAULT_INTERFACE_ID = 0
+  DEFAULT_DEVICE_INDEX = 0
 
   OFF = 0x00
   COLORS = [
     GREEN = 0x01,
     RED = 0x02,
-    YELLOW = 0x04,
-    GREEN_AND_RED = GREEN | RED,
-    GREEN_AND_YELLOW = GREEN | YELLOW,
-    RED_AND_YELLOW = RED | YELLOW,
-    ALL = GREEN_AND_YELLOW_AND_RED = GREEN | YELLOW | RED,
+    BLUE = 0x04, # this is yellow on the RGY model
+    YELLOW = GREEN | RED,
+    CYAN = GREEN | BLUE,
+    PURPLE = RED | BLUE,
+    WHITE = GREEN | BLUE | RED,
   ]
-
-  def initialize(opts = {})
-    @vendor_id = opts[:vendor_id] || DEFAULT_VENDOR_ID
-    @product_id = opts[:product_id] || DEFAULT_PRODUCT_ID
-    @interface_id = opts[:interface_id] || DEFAULT_INTERFACE_ID
-    @device = USB.devices.find {|device| device.idVendor == @vendor_id && device.idProduct == @product_id}
-    raise "Unable to find device" unless @device
-  end
-
-  def close
-    handle.release_interface(@interface_id)
-    handle.usb_close
-    @handle = nil
-  end
 
   def self.open(*args, &block)
     light = new(*args)
@@ -52,9 +39,26 @@ class Delcom
     end
   end
 
+  def initialize(opts = {})
+    @vendor_id = opts[:vendor_id] || DEFAULT_VENDOR_ID
+    @product_id = opts[:product_id] || DEFAULT_PRODUCT_ID
+    @device_index = opts[:device_index] || DEFAULT_DEVICE_INDEX
+    @interface_id = opts[:interface_id] || DEFAULT_INTERFACE_ID
+    @device = USB.devices.select {|device| device.idVendor == @vendor_id && device.idProduct == @product_id}[@device_index]
+    raise "Unable to find device" unless @device
+  end
+
+  def close
+    handle.release_interface(@interface_id)
+    handle.usb_close
+    @handle = nil
+  end
+
   def set(data)
     handle.usb_control_msg(0x21, 0x09, 0x0635, 0x000, "\x65\x0C#{[data].pack('C')}\xFF\x00\x00\x00\x00", 0)
   end
+
+  private
 
   def handle
     return @handle if @handle
@@ -76,21 +80,23 @@ class Delcom
 end
 
 if $0 == __FILE__ # example
-  #require 'delcom_904008'
   require 'timeout'
 
-  Delcom.open do |light|
+  light1 = Delcom.new(:device_index => 0)
+  light2 = Delcom.new(:device_index => 1)
+  begin
     begin
       Timeout::timeout(5) do
-        i = 0
         loop do
-          light.set Delcom::COLORS[i]
-          i = (i + 1) % Delcom::COLORS.size
-          sleep 0.1
+          light1.set Delcom::PURPLE
+          light2.set Delcom::YELLOW
+          light1, light2 = light2, light1
+          sleep 0.5
         end
       end
     rescue Timeout::Error
-      light.set(Delcom::OFF)
+      light1.set(Delcom::OFF)
+      light2.set(Delcom::OFF)
     end
   end
 end
