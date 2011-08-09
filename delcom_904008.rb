060cb159ea35dc26c562a3dcaacf6e84bbbae406
Gregory McIntyre <blue.puyo@gmail.com>
2011-08-09 14:22:42 +1000

    Updated API to support all RGY colour combinations.

--- delcom_904008.rb
+++ delcom_904008.rb
@@ -1,69 +1,96 @@
 #!/usr/bin/env ruby
 #
-# Author: Ian Leitch <ian@envato.com>
-# Copyright 2010 Envato
+# Original author: Ian Leitch <ian@envato.com>, Copyright 2010 Envato
+# Extra light colour combinations: Gregory McIntyre <blue.puyo@gmail.com>
+#
+# You'll need to install ruby-usb:
+#
+#   gem install ruby-usb
+#
+# Only pass :vendor_id, :product_id or :interface_id to Delcom.open if you need
+# to override their defaults.
 
+require 'rubygems' rescue nil
 require 'usb'
 
-module Delcom
-  class SignalIndicator
-    VENDOR_ID = 0x0fc5
-    PRODUCT_ID = 0xb080
-    INTERFACE_ID = 0
+class Delcom
+  DEFAULT_VENDOR_ID = 0x0fc5
+  DEFAULT_PRODUCT_ID = 0xb080
+  DEFAULT_INTERFACE_ID = 0
 
-    OFF = "\x00"
-    GREEN = "\x01"
-    RED = "\x02"
-    YELLOW = "\x04"
+  OFF = 0x00
+  COLORS = [
+    GREEN = 0x01,
+    RED = 0x02,
+    YELLOW = 0x04,
+    GREEN_AND_RED = GREEN | RED,
+    GREEN_AND_YELLOW = GREEN | YELLOW,
+    RED_AND_YELLOW = RED | YELLOW,
+    ALL = GREEN_AND_YELLOW_AND_RED = GREEN | YELLOW | RED,
+  ]
 
-    def initialize
-      @device = USB.devices.find {|device| device.idVendor == VENDOR_ID && device.idProduct == PRODUCT_ID}
-      raise "Unable to find device" unless @device
+  def initialize(opts = {})
+    @vendor_id = opts[:vendor_id] || DEFAULT_VENDOR_ID
+    @product_id = opts[:product_id] || DEFAULT_PRODUCT_ID
+    @interface_id = opts[:interface_id] || DEFAULT_INTERFACE_ID
+    @device = USB.devices.find {|device| device.idVendor == @vendor_id && device.idProduct == @product_id}
+    raise "Unable to find device" unless @device
+  end
+
+  def close
+    handle.release_interface(@interface_id)
+    handle.usb_close
+    @handle = nil
+  end
+
+  def self.open(*args, &block)
+    light = new(*args)
+    begin
+      block.call(light)
+    ensure
+      light.close
     end
+  end
 
-    def green
-      msg(GREEN)
-    end
+  def set(data)
+    handle.usb_control_msg(0x21, 0x09, 0x0635, 0x000, "\x65\x0C#{[data].pack('C')}\xFF\x00\x00\x00\x00", 0)
+  end
 
-    def yellow
-      msg(YELLOW)
-    end
-
-    def red
-      msg(RED)
-    end
-
-    def off
-      msg(OFF)
-    end
-
-    def close
-      handle.release_interface(INTERFACE_ID)
-      handle.usb_close
-      @handle = nil
-    end
-
-  private
-    def msg(data)
-      handle.usb_control_msg(0x21, 0x09, 0x0635, 0x000, "\x65\x0C#{data}\xFF\x00\x00\x00\x00", 0)
-    end
-
-    def handle
-      return @handle if @handle
-      @handle = @device.usb_open
-      begin
-        # ruby-usb bug: the arity of rusb_detach_kernel_driver_np isn't defined correctly, it should only accept a single argument.
-        if USB::DevHandle.instance_method(:usb_detach_kernel_driver_np).arity == 2
-          @handle.usb_detach_kernel_driver_np(INTERFACE_ID, INTERFACE_ID)
-        else
-          @handle.usb_detach_kernel_driver_np(INTERFACE_ID)
-        end
-      rescue Errno::ENODATA => e
-        # Already detached
+  def handle
+    return @handle if @handle
+    @handle = @device.usb_open
+    begin
+      # ruby-usb bug: the arity of rusb_detach_kernel_driver_np isn't defined correctly, it should only accept a single argument
+      if USB::DevHandle.instance_method(:usb_detach_kernel_driver_np).arity == 2
+        @handle.usb_detach_kernel_driver_np(@interface_id, @interface_id)
+      else
+        @handle.usb_detach_kernel_driver_np(@interface_id)
       end
-      @handle.set_configuration(@device.configurations.first)
-      @handle.claim_interface(INTERFACE_ID)
-      @handle
+    rescue Errno::ENODATA => e
+      # already detached
+    end
+    @handle.set_configuration(@device.configurations.first)
+    @handle.claim_interface(@interface_id)
+    @handle
+  end
+end
+
+if $0 == __FILE__ # example
+  #require 'delcom_904008'
+  require 'timeout'
+
+  Delcom.open do |light|
+    begin
+      Timeout::timeout(5) do
+        i = 0
+        loop do
+          light.set Delcom::COLORS[i]
+          i = (i + 1) % Delcom::COLORS.size
+          sleep 0.1
+        end
+      end
+    rescue Timeout::Error
+      light.set(Delcom::OFF)
     end
   end
 end
